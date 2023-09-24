5ba35b61bc5b6e186424d85fa8c843501f713f26
ironfprcefeild <lolten278@gmail.com>
2023-09-24 08:42:38 -0400

    improvements to gate link network

--- src/main/java/net/ironf/bitsnbytes/backend/circuitry/basicLinkingBlockEntity.java
+++ src/main/java/net/ironf/bitsnbytes/backend/circuitry/basicLinkingBlockEntity.java
@@ -8,6 +8,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
+import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Optional;
 
@@ -52,6 +53,54 @@ public class basicLinkingBlockEntity extends SmartBlockEntity {
         }
         return Optional.empty();
     }
+
+    public boolean isNetWorkValid(){
+        return testForValidNetwork(this.getBlockPos());
+    }
+    public boolean testForValidNetwork(BlockPos testingPos){
+        boolean toReturn = true;
+        //Loop through Block Pos in this Level
+        for (BlockPos bp : this.outputs){
+
+            //Return if match is found
+            if (bp == testingPos){
+                return false;
+            }
+            //Get Linked Block Pos
+            Optional<basicLinkingBlockEntity> linkAt = getLinkerAt(bp);
+            //Check Look through the linked blocks outputs
+            if (linkAt.isPresent() && linkAt.get().getOutputCount() > 0 && !linkAt.get().testForValidNetwork(testingPos)){
+                 return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void remove() {
+        for (BlockPos bp : this.outputs){
+            Optional<basicLinkingBlockEntity> linkAt = getLinkerAt(bp);
+            linkAt.ifPresent(linkingBlock -> linkingBlock.terminateConnection(bp));
+        }
+        for (BlockPos bp : this.inputs){
+            Optional<basicLinkingBlockEntity> linkAt = getLinkerAt(bp);
+            linkAt.ifPresent(basicLinkingBlockEntity -> basicLinkingBlockEntity.terminateConnection(bp));
+        }
+    }
+
+    public void terminateConnection(BlockPos toTerminate){
+        for (int i = 0; i < getInputCount(); i++){
+            if (this.inputs[i] == toTerminate){
+                this.inputs[i] = null;
+            }
+        }
+        for (int i = 0; i < getOutputCount(); i++){
+            if (this.outputs[i] == toTerminate){
+                this.outputs[i] = null;
+            }
+        }
+    }
+
 
     @Override
     protected void read(CompoundTag tag, boolean clientPacket) {
